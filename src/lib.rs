6fc962f5886acf6eb3279996144da848dccfb70c
Johanna <jsorngard@gmail.com>
2024-12-16 14:59:27 +0100

    Add tests of char sort and correct doctests

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,7 @@
 //! Sort an array by value:
 //!
 //! ```
-//! use const_sort::sort_i32_array;
+//! use compile_time_sort::sort_i32_array;
 //!
 //! const ARRAY: [i32; 5] = [-3, 3, 2, i32::MAX, 0];
 //! const SORTED_ARRAY: [i32; 5] = sort_i32_array(ARRAY);
@@ -24,7 +24,7 @@
 //! Sort an array by reference:
 //!
 //! ```
-//! use const_sort::sort_i32_slice;
+//! use compile_time_sort::sort_i32_slice;
 //!
 //! const SORTED_ARRAY: [i32; 5] = {
 //!     let mut arr = [5, i32::MIN, 0, -2, 0];
@@ -301,5 +301,13 @@ mod test {
 
         assert_eq!(SORTED_ARR, [-50, -2, 0, 5, 50]);
         assert_eq!(sorted_arr, [i8::MIN; 100]);
+    }
+
+    #[test]
+    fn test_char_sort() {
+        const ARR: [char; 4] = ['a', '#', '\n', 'A'];
+        const SORTED_ARR: [char; 4] = sort_char_array(ARR);
+
+        assert_eq!(SORTED_ARR, ['\n', '#', 'A', 'a'])
     }
 }
