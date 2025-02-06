41c7eb1027779950d8caa5a2e9cef7671b9013c7
Johanna Sörngård <jsorngard@gmail.com>
2025-02-06 20:11:01 +0100

    Add tests of all functions using macros

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,91 +1,106 @@
 use compile_time_sort::{
-    into_sorted_bool_array, into_sorted_char_array, into_sorted_i128_array, into_sorted_i32_array,
-    into_sorted_i8_array, into_sorted_u32_array, into_sorted_u64_array, into_sorted_u8_array,
+    into_sorted_bool_array, into_sorted_char_array, into_sorted_i128_array, into_sorted_i16_array,
+    into_sorted_i32_array, into_sorted_i64_array, into_sorted_i8_array, into_sorted_u128_array,
+    into_sorted_u16_array, into_sorted_u32_array, into_sorted_u64_array, into_sorted_u8_array,
 };
 
 #[cfg(feature = "sort_slices")]
-use compile_time_sort::{sort_bool_slice, sort_i32_slice, sort_i8_slice, sort_u32_slice};
+use compile_time_sort::{
+    sort_bool_slice, sort_i128_slice, sort_i16_slice, sort_i32_slice, sort_i64_slice,
+    sort_i8_slice, sort_u128_slice, sort_u16_slice, sort_u32_slice, sort_u64_slice, sort_u8_slice,
+};
 
-#[test]
-fn test_sort_u64() {
-    const REV_ARRAY: [u64; 3] = [3, 2, 1];
-    const SORTED_REV_ARRAY: [u64; 3] = into_sorted_u64_array(REV_ARRAY);
-    const CONST_ARRAY: [u64; 3] = [2, 2, 2];
-    const SORTED_CONST_ARRAY: [u64; 3] = into_sorted_u64_array(CONST_ARRAY);
+macro_rules! test_unsigned_integer {
+    ($tpe:ty, $fn_name:ident, $array_sort_name:ident, $slice_sort_name:ident) => {
+        #[test]
+        fn $fn_name() {
+            const REV_ARRAY: [$tpe; 3] = [3, 2, 1];
+            const SORTED_REV_ARRAY: [$tpe; 3] = $array_sort_name(REV_ARRAY);
+            const CONST_ARRAY: [$tpe; 3] = [2, 2, 2];
+            const SORTED_CONST_ARRAY: [$tpe; 3] = $array_sort_name(CONST_ARRAY);
 
-    assert_eq!(SORTED_REV_ARRAY, [1, 2, 3]);
-    assert_eq!(SORTED_CONST_ARRAY, [2, 2, 2]);
-}
+            const EMPTY_ARRAY: [$tpe; 0] = [];
+            const SORTED_EMPTY_ARRAY: [$tpe; 0] = $array_sort_name(EMPTY_ARRAY);
+            const SINGLETON_ARRAY: [$tpe; 1] = [1];
+            const SORTED_SINGLETON_ARRAY: [$tpe; 1] = $array_sort_name(SINGLETON_ARRAY);
 
-#[test]
-fn test_sort_i128() {
-    const REV_ARRAY: [i128; 3] = [3, 2, 1];
-    const SORTED_REV_ARRAY: [i128; 3] = into_sorted_i128_array(REV_ARRAY);
-    const CONST_ARRAY: [i128; 3] = [2, 2, 2];
-    const SORTED_CONST_ARRAY: [i128; 3] = into_sorted_i128_array(CONST_ARRAY);
-    const ARRAY_WITH_NEGATIVES: [i128; 3] = [0, -1, 2];
-    const SORTED_ARRAY_WITH_NEGATIVES: [i128; 3] = into_sorted_i128_array(ARRAY_WITH_NEGATIVES);
+            assert_eq!(SORTED_EMPTY_ARRAY, []);
+            assert_eq!(SORTED_SINGLETON_ARRAY, [1]);
 
-    assert_eq!(SORTED_REV_ARRAY, [1, 2, 3]);
-    assert_eq!(SORTED_CONST_ARRAY, [2, 2, 2]);
-    assert_eq!(SORTED_ARRAY_WITH_NEGATIVES, [-1, 0, 2]);
-}
+            assert_eq!(SORTED_REV_ARRAY, [1, 2, 3]);
+            assert_eq!(SORTED_CONST_ARRAY, [2, 2, 2]);
 
-#[test]
-fn test_sort_i32() {
-    const REV_ARRAY: [i32; 3] = [3, 2, 1];
-    const SORTED_REV_ARRAY: [i32; 3] = into_sorted_i32_array(REV_ARRAY);
-    const CONST_ARRAY: [i32; 3] = [2, 2, 2];
-    const SORTED_CONST_ARRAY: [i32; 3] = into_sorted_i32_array(CONST_ARRAY);
-    const ARRAY_WITH_NEGATIVES: [i32; 3] = [0, -1, 2];
-    const SORTED_ARRAY_WITH_NEGATIVES: [i32; 3] = into_sorted_i32_array(ARRAY_WITH_NEGATIVES);
+            #[cfg(feature = "sort_slices")]
+            {
+                const SORTED_SLICE: [$tpe; 3] = {
+                    let mut arr = REV_ARRAY;
+                    $slice_sort_name(&mut arr);
+                    arr
+                };
 
-    #[cfg(feature = "sort_slices")]
-    const SORTED_SLICE: [i32; 3] = {
-        let mut arr = REV_ARRAY;
-        sort_i32_slice(&mut arr);
-        arr
+                assert_eq!(SORTED_SLICE, SORTED_REV_ARRAY);
+            }
+        }
     };
-
-    assert_eq!(SORTED_REV_ARRAY, [1, 2, 3]);
-    assert_eq!(SORTED_CONST_ARRAY, [2, 2, 2]);
-    assert_eq!(SORTED_ARRAY_WITH_NEGATIVES, [-1, 0, 2]);
-    #[cfg(feature = "sort_slices")]
-    assert_eq!(SORTED_SLICE, SORTED_REV_ARRAY);
 }
 
-#[test]
-fn test_edge_cases() {
-    const EMPTY_ARRAY: [i32; 0] = [];
-    const SORTED_EMPTY_ARRAY: [i32; 0] = into_sorted_i32_array(EMPTY_ARRAY);
-    const SINGLETON_ARRAY: [i32; 1] = [1];
-    const SORTED_SINGLETON_ARRAY: [i32; 1] = into_sorted_i32_array(SINGLETON_ARRAY);
-    const ARRAY_WITH_NEGATIVES: [i32; 3] = [0, -1, 2];
-    const SORTED_ARRAY_WITH_NEGATIVES: [i32; 3] = into_sorted_i32_array(ARRAY_WITH_NEGATIVES);
+macro_rules! test_signed_integer {
+    ($tpe:ty, $fn_name:ident, $array_sort_name:ident, $slice_sort_name:ident) => {
+        #[test]
+        fn $fn_name() {
+            const REV_ARRAY: [$tpe; 3] = [3, 2, 1];
+            const SORTED_REV_ARRAY: [$tpe; 3] = $array_sort_name(REV_ARRAY);
+            const CONST_ARRAY: [$tpe; 3] = [2, 2, 2];
+            const SORTED_CONST_ARRAY: [$tpe; 3] = $array_sort_name(CONST_ARRAY);
+            const ARRAY_WITH_NEGATIVES: [$tpe; 3] = [0, -1, 2];
+            const SORTED_ARRAY_WITH_NEGATIVES: [$tpe; 3] = $array_sort_name(ARRAY_WITH_NEGATIVES);
 
-    assert_eq!(SORTED_EMPTY_ARRAY, []);
-    assert_eq!(SORTED_SINGLETON_ARRAY, [1]);
-    assert_eq!(SORTED_ARRAY_WITH_NEGATIVES, [-1, 0, 2]);
-}
+            const EMPTY_ARRAY: [$tpe; 0] = [];
+            const SORTED_EMPTY_ARRAY: [$tpe; 0] = $array_sort_name(EMPTY_ARRAY);
+            const SINGLETON_ARRAY: [$tpe; 1] = [1];
+            const SORTED_SINGLETON_ARRAY: [$tpe; 1] = $array_sort_name(SINGLETON_ARRAY);
 
-#[test]
-fn test_sort_u32() {
-    const REV_ARRAY: [u32; 3] = [3, 2, 1];
-    const SORTED_REV_ARRAY: [u32; 3] = into_sorted_u32_array(REV_ARRAY);
-    const CONST_ARRAY: [u32; 3] = [2, 2, 2];
-    const SORTED_CONST_ARRAY: [u32; 3] = into_sorted_u32_array(CONST_ARRAY);
+            assert_eq!(SORTED_EMPTY_ARRAY, []);
+            assert_eq!(SORTED_SINGLETON_ARRAY, [1]);
 
-    #[cfg(feature = "sort_slices")]
-    const SORTED_SLICE: [u32; 3] = {
-        let mut arr = REV_ARRAY;
-        sort_u32_slice(&mut arr);
-        arr
+            assert_eq!(SORTED_REV_ARRAY, [1, 2, 3]);
+            assert_eq!(SORTED_CONST_ARRAY, [2, 2, 2]);
+            assert_eq!(SORTED_ARRAY_WITH_NEGATIVES, [-1, 0, 2]);
+
+            #[cfg(feature = "sort_slices")]
+            {
+                const SORTED_SLICE: [$tpe; 3] = {
+                    let mut arr = ARRAY_WITH_NEGATIVES;
+                    $slice_sort_name(&mut arr);
+                    arr
+                };
+
+                assert_eq!(SORTED_SLICE, SORTED_ARRAY_WITH_NEGATIVES);
+            }
+        }
     };
+}
 
-    assert_eq!(SORTED_REV_ARRAY, [1, 2, 3]);
-    assert_eq!(SORTED_CONST_ARRAY, [2, 2, 2]);
-    #[cfg(feature = "sort_slices")]
-    assert_eq!(SORTED_SLICE, SORTED_REV_ARRAY)
+test_unsigned_integer! {u8, test_sort_u8, into_sorted_u8_array, sort_u8_slice}
+test_unsigned_integer! {u16, test_sort_u16, into_sorted_u16_array, sort_u16_slice}
+test_unsigned_integer! {u32, test_sort_u32, into_sorted_u32_array, sort_u32_slice}
+test_unsigned_integer! {u64, test_sort_u64, into_sorted_u64_array, sort_u64_slice}
+test_unsigned_integer! {
+    u128,
+    test_sort_u128,
+    into_sorted_u128_array,
+    sort_u128_slice
+}
+
+test_signed_integer! {i8, test_sort_i8, into_sorted_i8_array, sort_i8_slice}
+test_signed_integer! {i16, test_sort_i16, into_sorted_i16_array, sort_i16_slice}
+test_signed_integer! {i32, test_sort_i32, into_sorted_i32_array, sort_i32_slice}
+test_signed_integer! {i64, test_sort_i64, into_sorted_i64_array, sort_i64_slice}
+test_signed_integer! {
+    i128,
+    test_sort_i128,
+    into_sorted_i128_array,
+    sort_i128_slice
 }
 
 #[test]
@@ -103,31 +118,6 @@ fn test_sort_bool() {
     assert_eq!(SORTED_ARR, [false, false, true, true]);
     #[cfg(feature = "sort_slices")]
     assert_eq!(SORTED_SLICE, [false, false, true, true]);
-}
-
-#[test]
-fn test_u8_sort() {
-    const ARR: [u8; 5] = [8, 1, u8::MAX, 5, 0];
-    const SORTED_ARR: [u8; 5] = into_sorted_u8_array(ARR);
-
-    assert_eq!(SORTED_ARR, [0, 1, 5, 8, u8::MAX]);
-}
-
-#[test]
-fn test_i8_sort() {
-    const ARR: [i8; 5] = [-2, 50, 0, 5, -50];
-    const SORTED_ARR: [i8; 5] = into_sorted_i8_array(ARR);
-
-    #[cfg(feature = "sort_slices")]
-    const SORTED_SLICE: [i8; 100] = {
-        let mut arr = [i8::MIN; 100];
-        sort_i8_slice(&mut arr);
-        arr
-    };
-
-    assert_eq!(SORTED_ARR, [-50, -2, 0, 5, 50]);
-    #[cfg(feature = "sort_slices")]
-    assert_eq!(SORTED_SLICE, [i8::MIN; 100]);
 }
 
 #[test]
