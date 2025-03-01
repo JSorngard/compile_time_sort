6481464cbdaa93cd92c3223fccf3e535bdbc81ed
Johanna Sörngård <44257381+JSorngard@users.noreply.github.com>
2025-03-01 22:54:39 +0100

    Fix sort bug (#12)
    * Found a bug in the sorting algorithm for u16
    
    * clearer print in test
    
    * print the location where the wrong values end up
    
    * Change impl of quicksort
    
    * Add a min to the doc example
    
    * Generate doc example through macro
    
    * Remove the doc example
    
    * reuse the already made random array
    
    * Fix tests of array
    
    * Modularize tests
    
    * simplify swap
    
    * use the is_sorted function
    
    * use `array::from_fn`
    
    * Small demo of using quickcheck
    
    * Add quickcheck test of all the slice sorting methods
    
    * Fix the sorting bug finally!
    
    * Use `rand` version 0.8 in order to be compatible with the stated MSRV
    
    * Use a version known to worl
    
    * Raised MSRV to 1.60.0 and bumped minor version number
    
    * Unbumped msrv
    
    * Unbump minor version number, just bump patch
    
    * Correct version number in changelog
    
    * Dont expect, just in case

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,3 +1,7 @@
+#[cfg(feature = "sort_slices")]
+use quickcheck::quickcheck;
+use rand::{rngs::SmallRng, Rng, SeedableRng};
+
 use compile_time_sort::{
     into_sorted_bool_array, into_sorted_char_array, into_sorted_i128_array, into_sorted_i16_array,
     into_sorted_i32_array, into_sorted_i64_array, into_sorted_i8_array, into_sorted_u128_array,
@@ -6,9 +10,23 @@ use compile_time_sort::{
 
 #[cfg(feature = "sort_slices")]
 use compile_time_sort::{
-    sort_bool_slice, sort_i128_slice, sort_i16_slice, sort_i32_slice, sort_i64_slice,
-    sort_i8_slice, sort_u128_slice, sort_u16_slice, sort_u32_slice, sort_u64_slice, sort_u8_slice,
+    sort_bool_slice, sort_char_slice, sort_i128_slice, sort_i16_slice, sort_i32_slice,
+    sort_i64_slice, sort_i8_slice, sort_isize_slice, sort_u128_slice, sort_u16_slice,
+    sort_u32_slice, sort_u64_slice, sort_u8_slice, sort_usize_slice,
 };
+
+#[cfg(feature = "sort_slices")]
+macro_rules! quickcheck_slice_sort {
+    ($tpe:ty, $quickcheck_fn:ident, $sort_fn:ident) => {
+        quickcheck! {
+            fn $quickcheck_fn(vec: Vec<$tpe>) -> bool {
+                let mut vec = vec;
+                $sort_fn(&mut vec);
+                vec.is_sorted()
+            }
+        }
+    };
+}
 
 macro_rules! test_unsigned_integer {
     ($tpe:ty, $fn_name:ident, $array_sort_name:ident, $slice_sort_name:ident) => {
@@ -16,19 +34,40 @@ macro_rules! test_unsigned_integer {
         fn $fn_name() {
             const REV_ARRAY: [$tpe; 3] = [3, 2, 1];
             const SORTED_REV_ARRAY: [$tpe; 3] = $array_sort_name(REV_ARRAY);
+
             const CONST_ARRAY: [$tpe; 3] = [2, 2, 2];
             const SORTED_CONST_ARRAY: [$tpe; 3] = $array_sort_name(CONST_ARRAY);
 
+            const INCORRECT_LAST: [$tpe; 4] = [1, 1, 1, 0];
+            const SORTED_INCORRECT_LAST: [$tpe; 4] = $array_sort_name(INCORRECT_LAST);
+
+            const JUST_ZEROS_ARRAY: [$tpe; 100] = [0; 100];
+            const SORTED_JUST_ZEROS_ARRAY: [$tpe; 100] = $array_sort_name(JUST_ZEROS_ARRAY);
+
             const EMPTY_ARRAY: [$tpe; 0] = [];
             const SORTED_EMPTY_ARRAY: [$tpe; 0] = $array_sort_name(EMPTY_ARRAY);
+
             const SINGLETON_ARRAY: [$tpe; 1] = [1];
             const SORTED_SINGLETON_ARRAY: [$tpe; 1] = $array_sort_name(SINGLETON_ARRAY);
 
-            assert_eq!(SORTED_EMPTY_ARRAY, []);
-            assert_eq!(SORTED_SINGLETON_ARRAY, [1]);
+            assert!(SORTED_EMPTY_ARRAY.is_sorted());
 
-            assert_eq!(SORTED_REV_ARRAY, [1, 2, 3]);
-            assert_eq!(SORTED_CONST_ARRAY, [2, 2, 2]);
+            assert!(SORTED_SINGLETON_ARRAY.is_sorted());
+
+            assert!(SORTED_REV_ARRAY.is_sorted());
+
+            assert!(SORTED_CONST_ARRAY.is_sorted());
+
+            assert!(SORTED_INCORRECT_LAST.is_sorted());
+
+            assert!(SORTED_JUST_ZEROS_ARRAY.is_sorted());
+
+            let mut rng = SmallRng::from_seed([0b01010101; 32]);
+
+            let random_array: [$tpe; 500] = core::array::from_fn(|_| rng.gen());
+
+            let sorted_array = $array_sort_name(random_array);
+            assert!(sorted_array.is_sorted());
 
             #[cfg(feature = "sort_slices")]
             {
@@ -38,46 +77,44 @@ macro_rules! test_unsigned_integer {
                     arr
                 };
 
-                assert_eq!(SORTED_SLICE, SORTED_REV_ARRAY);
+                assert!(SORTED_SLICE.is_sorted());
+
+                let sorted_array = {
+                    let mut arr = random_array;
+                    $slice_sort_name(&mut arr);
+                    arr
+                };
+
+                assert!(sorted_array.is_sorted());
             }
         }
     };
 }
 
 macro_rules! test_signed_integer {
-    ($tpe:ty, $fn_name:ident, $array_sort_name:ident, $slice_sort_name:ident) => {
+    ($tpe:ty, $negative_fn_name:ident, $fn_name:ident, $array_sort_name:ident, $slice_sort_name:ident) => {
         #[test]
-        fn $fn_name() {
-            const REV_ARRAY: [$tpe; 3] = [3, 2, 1];
-            const SORTED_REV_ARRAY: [$tpe; 3] = $array_sort_name(REV_ARRAY);
-            const CONST_ARRAY: [$tpe; 3] = [2, 2, 2];
-            const SORTED_CONST_ARRAY: [$tpe; 3] = $array_sort_name(CONST_ARRAY);
+        fn $negative_fn_name() {
             const ARRAY_WITH_NEGATIVES: [$tpe; 3] = [0, -1, 2];
             const SORTED_ARRAY_WITH_NEGATIVES: [$tpe; 3] = $array_sort_name(ARRAY_WITH_NEGATIVES);
 
-            const EMPTY_ARRAY: [$tpe; 0] = [];
-            const SORTED_EMPTY_ARRAY: [$tpe; 0] = $array_sort_name(EMPTY_ARRAY);
-            const SINGLETON_ARRAY: [$tpe; 1] = [1];
-            const SORTED_SINGLETON_ARRAY: [$tpe; 1] = $array_sort_name(SINGLETON_ARRAY);
-
-            assert_eq!(SORTED_EMPTY_ARRAY, []);
-            assert_eq!(SORTED_SINGLETON_ARRAY, [1]);
-
-            assert_eq!(SORTED_REV_ARRAY, [1, 2, 3]);
-            assert_eq!(SORTED_CONST_ARRAY, [2, 2, 2]);
-            assert_eq!(SORTED_ARRAY_WITH_NEGATIVES, [-1, 0, 2]);
+            assert!(SORTED_ARRAY_WITH_NEGATIVES.is_sorted());
 
             #[cfg(feature = "sort_slices")]
             {
-                const SORTED_SLICE: [$tpe; 3] = {
+                const ARRAY_WITH_NEGATIVES: [$tpe; 3] = [0, -1, 2];
+                const SORTED_ARRAY_WITH_NEGATIVES: [$tpe; 3] = {
                     let mut arr = ARRAY_WITH_NEGATIVES;
                     $slice_sort_name(&mut arr);
                     arr
                 };
 
-                assert_eq!(SORTED_SLICE, SORTED_ARRAY_WITH_NEGATIVES);
+                assert!(SORTED_ARRAY_WITH_NEGATIVES.is_sorted());
             }
         }
+
+        // Also run all the tests for unsigned integers on the signed integers
+        test_unsigned_integer! {$tpe, $fn_name, $array_sort_name, $slice_sort_name}
     };
 }
 
@@ -92,16 +129,46 @@ test_unsigned_integer! {
     sort_u128_slice
 }
 
-test_signed_integer! {i8, test_sort_i8, into_sorted_i8_array, sort_i8_slice}
-test_signed_integer! {i16, test_sort_i16, into_sorted_i16_array, sort_i16_slice}
-test_signed_integer! {i32, test_sort_i32, into_sorted_i32_array, sort_i32_slice}
-test_signed_integer! {i64, test_sort_i64, into_sorted_i64_array, sort_i64_slice}
+test_signed_integer! {i8, test_small_negative_sort_i8, test_i8, into_sorted_i8_array, sort_i8_slice}
+test_signed_integer! {i16, test_small_negative_sort_i16, test_i16, into_sorted_i16_array, sort_i16_slice}
+test_signed_integer! {i32, test_small_negative_sort_i32, test_i32, into_sorted_i32_array, sort_i32_slice}
+test_signed_integer! {i64, test_small_negative_sort_i64, test_i64, into_sorted_i64_array, sort_i64_slice}
 test_signed_integer! {
     i128,
-    test_sort_i128,
+    test_small_negative_sort_i128,
+    test_i128,
     into_sorted_i128_array,
     sort_i128_slice
 }
+
+#[cfg(feature = "sort_slices")]
+quickcheck_slice_sort! {u8, quickcheck_u8_slice, sort_u8_slice}
+#[cfg(feature = "sort_slices")]
+quickcheck_slice_sort! {u16, quickcheck_u16_slice, sort_u16_slice}
+#[cfg(feature = "sort_slices")]
+quickcheck_slice_sort! {u32, quickcheck_u32_slice, sort_u32_slice}
+#[cfg(feature = "sort_slices")]
+quickcheck_slice_sort! {u64, quickcheck_u64_slice, sort_u64_slice}
+#[cfg(feature = "sort_slices")]
+quickcheck_slice_sort! {u128, quickcheck_u128_slice, sort_u128_slice}
+#[cfg(feature = "sort_slices")]
+quickcheck_slice_sort! {usize, quickcheck_usize_slice, sort_usize_slice}
+#[cfg(feature = "sort_slices")]
+quickcheck_slice_sort! {i8, quickcheck_i8_slice, sort_i8_slice}
+#[cfg(feature = "sort_slices")]
+quickcheck_slice_sort! {i16, quickcheck_i16_slice, sort_i16_slice}
+#[cfg(feature = "sort_slices")]
+quickcheck_slice_sort! {i32, quickcheck_i32_slice, sort_i32_slice}
+#[cfg(feature = "sort_slices")]
+quickcheck_slice_sort! {i64, quickcheck_i64_slice, sort_i64_slice}
+#[cfg(feature = "sort_slices")]
+quickcheck_slice_sort! {i128, quickcheck_i128_slice, sort_i128_slice}
+#[cfg(feature = "sort_slices")]
+quickcheck_slice_sort! {isize, quickcheck_isize_slice, sort_isize_slice}
+#[cfg(feature = "sort_slices")]
+quickcheck_slice_sort! {char, quickcheck_char_slice, sort_char_slice}
+#[cfg(feature = "sort_slices")]
+quickcheck_slice_sort! {bool, quickcheck_bool_slice, sort_bool_slice}
 
 #[test]
 fn test_sort_bool() {
