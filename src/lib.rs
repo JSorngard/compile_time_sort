bf40f3e9659428744d196c8225ee519f2d62b0fb
Johanna Sörngård <44257381+JSorngard@users.noreply.github.com>
2025-03-06 18:04:17 +0100

    Generate docs for the macro generate functions (#28)
    * Generate docs for the macro generate functions
    
    * Add doc examples to the sorting functions that use counting sort
    
    * Remove unused parens from doc example
    
    * Bump version and add changes to log

--- src/lib.rs
+++ src/lib.rs
@@ -149,6 +149,15 @@ macro_rules! impl_const_quicksort {
                 const_array_quicksort!{$tpe, [<qsort_ $tpe _array>], [<partition_ $tpe _array>]}
 
                 #[doc = concat!("Sorts the given array of `", stringify!($tpe), "`s using the quicksort algorithm and returns it.")]
+                #[doc = ""]
+                #[doc = "# Example"]
+                #[doc = ""]
+                #[doc = "```"]
+                #[doc = concat!("# use compile_time_sort::", stringify!([<into_sorted_ $tpe _array>]), ";")]
+                #[doc = concat!("const SORTED_ARRAY: [", stringify!($tpe), "; 2] = ", stringify!([<into_sorted_ $tpe _array>]), "([", stringify!($tpe),"::MAX, ", stringify!($tpe), "::MIN]);")]
+                #[doc = ""]
+                #[doc = "assert!(SORTED_ARRAY.is_sorted());"]
+                #[doc = "```"]
                 pub const fn [<into_sorted_ $tpe _array>]<const N: usize>(array: [$tpe; N]) -> [$tpe; N] {
                     if N <= 1 {
                         return array;
@@ -160,6 +169,16 @@ macro_rules! impl_const_quicksort {
                 #[doc = concat!("Sorts the given slice of `", stringify!($tpe), "`s using the quicksort algorithm.")]
                 #[doc = ""]
                 #[doc = "This function is only available on Rust versions 1.83 and above."]
+                #[doc = ""]
+                #[doc = "# Example"]
+                #[doc = ""]
+                #[doc = "```"]
+                #[doc = concat!("# use compile_time_sort::", stringify!([<sort_ $tpe _slice>]), ";")]
+                #[doc = concat!("const ARRAY: [", stringify!($tpe), "; 2] = [", stringify!($tpe),"::MAX, ", stringify!($tpe), "::MIN];")]
+                #[doc = concat!("const SORTED_ARRAY: [", stringify!($tpe), "; 2]= {\n    let mut arr = ARRAY;\n    ", stringify!([<sort_ $tpe _slice>]), "(&mut arr);\n    arr\n};")]
+                #[doc = ""]
+                #[doc = "assert!(SORTED_ARRAY.is_sorted());"]
+                #[doc = "```"]
                 pub const fn [<sort_ $tpe _slice>](slice: &mut [$tpe]) {
                     [<qsort_ $tpe _slice>](slice);
                 }
@@ -186,6 +205,20 @@ impl_const_quicksort! {
 /// Sorts the given slice of `i8`s using the counting sort algorithm.
 ///
 /// This function is only available on Rust versions 1.83 and above.
+///
+/// # Example
+///
+/// ```
+/// # use compile_time_sort::sort_i8_slice;
+/// const ARRAY: [i8; 2] = [i8::MAX, i8::MIN];
+/// const SORTED_ARRAY: [i8; 2] = {
+///     let mut arr = ARRAY;
+///     sort_i8_slice(&mut arr);
+///     arr
+/// };
+///
+/// assert!(SORTED_ARRAY.is_sorted());
+/// ```
 pub const fn sort_i8_slice(slice: &mut [i8]) {
     if slice.is_empty() || slice.len() == 1 {
         return;
@@ -213,6 +246,15 @@ pub const fn sort_i8_slice(slice: &mut [i8]) {
 }
 
 /// Sorts the given array of `i8`s using the counting sort algorithm and returns it.
+///
+/// # Example
+///
+/// ```
+/// # use compile_time_sort::into_sorted_i8_array;
+/// const SORTED_ARRAY: [i8; 2] = into_sorted_i8_array([i8::MAX, i8::MIN]);
+///
+/// assert!(SORTED_ARRAY.is_sorted());
+/// ```
 pub const fn into_sorted_i8_array<const N: usize>(mut array: [i8; N]) -> [i8; N] {
     if N == 0 || N == 1 {
         return array;
@@ -245,6 +287,20 @@ pub const fn into_sorted_i8_array<const N: usize>(mut array: [i8; N]) -> [i8; N]
 /// Sorts the given slice of `u8`s using the counting sort algorithm.
 ///
 /// This function is only available on Rust versions 1.83 and above.
+///
+/// # Example
+///
+/// ```
+/// # use compile_time_sort::sort_u8_slice;
+/// const ARRAY: [u8; 2] = [u8::MAX, u8::MIN];
+/// const SORTED_ARRAY: [u8; 2] = {
+///     let mut arr = ARRAY;
+///     sort_u8_slice(&mut arr);
+///     arr
+/// };
+///
+/// assert!(SORTED_ARRAY.is_sorted());
+/// ```
 pub const fn sort_u8_slice(slice: &mut [u8]) {
     if slice.is_empty() || slice.len() == 1 {
         return;
@@ -272,6 +328,15 @@ pub const fn sort_u8_slice(slice: &mut [u8]) {
 }
 
 /// Sorts the given array of `u8`s using the counting sort algorithm and returns it.
+///
+/// # Example
+///
+/// ```
+/// # use compile_time_sort::into_sorted_u8_array;
+/// const SORTED_ARRAY: [u8; 2] = into_sorted_u8_array([u8::MAX, u8::MIN]);
+///
+/// assert!(SORTED_ARRAY.is_sorted());
+/// ```
 pub const fn into_sorted_u8_array<const N: usize>(mut array: [u8; N]) -> [u8; N] {
     if N == 0 || N == 1 {
         return array;
@@ -302,6 +367,20 @@ pub const fn into_sorted_u8_array<const N: usize>(mut array: [u8; N]) -> [u8; N]
 /// Sorts the given slice of `bool`s using the counting sort algorithm.
 ///
 /// This function is only available on Rust versions 1.83 and above.
+///
+/// # Example
+///
+/// ```
+/// # use compile_time_sort::sort_bool_slice;
+/// const ARRAY: [bool; 2] = [true, false];
+/// const SORTED_ARRAY: [bool; 2] = {
+///     let mut arr = ARRAY;
+///     sort_bool_slice(&mut arr);
+///     arr
+/// };
+///
+/// assert!(SORTED_ARRAY.is_sorted());
+/// ```
 pub const fn sort_bool_slice(slice: &mut [bool]) {
     if slice.is_empty() || slice.len() == 1 {
         return;
@@ -329,6 +408,15 @@ pub const fn sort_bool_slice(slice: &mut [bool]) {
 }
 
 /// Sorts the given array of `bool`s using the counting sort algorithm and returns it.
+///
+/// # Example
+///
+/// ```
+/// # use compile_time_sort::into_sorted_bool_array;
+/// const SORTED_ARRAY: [bool; 2] = into_sorted_bool_array([true, false]);
+///
+/// assert!(SORTED_ARRAY.is_sorted());
+/// ```
 pub const fn into_sorted_bool_array<const N: usize>(mut array: [bool; N]) -> [bool; N] {
     if N == 0 || N == 1 {
         return array;
